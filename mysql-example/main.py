6fe1fbf326f11b702d2aa83c418402e0fb2aa262
YOUNJEI <kyj951211@gmail.com>
2022-10-25 10:32:03 +0900

    mysql - example

--- mysql-example/main.py
+++ mysql-example/main.py
@@ -5,20 +5,21 @@ if __name__ == '__main__':
     cursor = conn.cursor()
 
     cursor.execute('USE tooz')
-    #cursor.execute('CREATE TABLE test (id int primary key, name varchar(255))')
+    #cursor.execute('CREATE TABLE test2 (id int primary key, name varchar(255), money int, weight double, time date)')
 
-    sql = 'INSERT INTO test (name) VALUES (%s)'
-    cursor.execute(sql, ('test',))
-    cursor.execute(sql, ('test',))
-    cursor.execute(sql, ('test',))
+    sql = 'INSERT INTO test2 (name, money, weight, time) VALUES (%s, %s, %s, %s)'
+    cursor.execute(sql, ('test', '15352', '69.2', '2022/10/25'))
+    cursor.execute(sql, ('test', '-100000', '50.2', '1995/12/11'))
 
-    sql = 'SELECT * FROM test where name = %s'
+    sql = 'SELECT * FROM test2 where name = %s'
     cursor.execute(sql, ('test',))
     result = cursor.fetchall()
 
     for data in result:
         print(data[0], end=" ")
         print(data[1], end=" ")
+        print(data[2], end=" ")
+        print(data[3], end=" ")
         print(data)
 
     conn.commit()
